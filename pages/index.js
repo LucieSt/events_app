51877161a4448eeb0494cc6f8cc3944257320d72
Lucie Boušková <strakova.91@gmail.com>
2023-04-18 20:56:04 +0200

    First commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+const Home = () => {
+	return (
+		<div className="container">
+			<header>
+				<nav>
+					<img />
+					<a href='/'>Home</a>
+					<a href='/events'>Events</a>
+					<a href='/about-us'>About us</a>
+				</nav>
+			</header>
+
+			<main>
+				<a href=''>
+					<img />
+					<h2>Events in London</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</a>
+
+				<a href=''>
+					<img />
+					<h2>Events in San Francisco</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</a>
+
+				<a href=''>
+					<img />
+					<h2>Events in Barcelona</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</a>
+			</main>
+
+			<footer>
+				<p>created in April 2023</p>
+			</footer>
+
+		</div>
+	)
+};
+
+export default Home;
