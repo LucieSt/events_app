f302d25c60f52c01d8210ee9a36a0f363ae878f5
Lucie Boušková <strakova.91@gmail.com>
2023-04-23 20:11:22 +0200

    template creation for each event of each city

--- next.config.js
+++ next.config.js
@@ -2,12 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    domains: ['images.unsplash.com', 'wembleypark.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/photo-*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wembleypark.com',
+        port: '',
+        pathname: '/media-*',
       },
     ],
   },
--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -1,18 +1,49 @@
-const EventPage = () => {
+import Image from "next/image";
+
+const EventPage = ({data}) => {
   return (
-    <div>
-      <a href="">
-        <img />
-        <h2>Our single event</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-      </a>
-    </div>
+    <>
+			{data.map((event) => {
+				return (
+					<div key={event.id}>
+						<Image src={event.image} alt={event.title} width={400} height={400} />
+						<h2>{event.title}</h2>
+						<p>{event.description}</p>
+					</div>
+				)
+			})}
+    </>
   );
 };
 
 export default EventPage;
+
+export async function getStaticPaths() {
+	const { allEvents } = await import("/data/data.json");
+	const allPaths = allEvents.map((event) => {
+    return {
+      params: {
+				cat: event.city.toString(),
+        id: event.id.toString(),
+      },
+    };
+  });
+
+	return {
+    paths: allPaths,
+    fallback: false,
+  };
+};
+
+export async function getStaticProps(context) {
+	const { allEvents } = await import("/data/data.json");
+	const events = allEvents.filter((event) => {
+    return event.id === context.params.id;
+  });
+
+  return {
+    props: {
+      data: events,
+    },
+  };
+};
