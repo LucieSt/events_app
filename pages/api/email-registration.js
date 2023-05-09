5f930a1e80472f277b2a9916e31daa20500236be
Lucie Boušková <strakova.91@gmail.com>
2023-05-09 20:33:07 +0200

    functionality is finished

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -5,11 +5,52 @@ function buildPath() {
   return path.join(process.cwd(), "data", "data.json");
 }
 
+function extractData(filePath) {
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
 export default function handler(req, res) {
-  console.log(buildPath());
   const { method } = req;
+
+  const filePath = buildPath();
+  const { events_categories, allEvents } = extractData(filePath);
+
+  if (!allEvents) {
+    return res.status(404).json({
+      message: "data not found",
+    });
+  }
+
   if (method === "POST") {
     const { email, eventId } = req.body;
+
+    if (!email | !email.includes("@")) {
+      res.status(422).json({ message: "Invalid email address" });
+      return;
+    }
+
+    const newAllEvents = allEvents.map((ev) => {
+      if (ev.id == eventId) {
+        if (ev.emails_registered.includes(email)) {
+          res
+            .status(409)
+            .json({ message: "This email has been already registered" });
+          return ev;
+        }
+        return {
+          ...ev,
+          emails_registered: [...ev.emails_registered, email],
+        };
+      }
+      return ev;
+    });
+
+    fs.writeFileSync(
+      filePath,
+      JSON.stringify({ events_categories, allEvents: newAllEvents })
+    );
 
     res.status(200).json({
       message: `You have been registered succesfully with an email: ${email}`,
--- src/components/events/eventCard.jsx
+++ src/components/events/eventCard.jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const SingleEvent = ({ data }) => {
+  const [message, setMessage] = useState("");
+
   const inputEmail = useRef();
   const router = useRouter();
 
@@ -10,6 +12,12 @@ const SingleEvent = ({ data }) => {
     e.preventDefault();
     const emailValue = inputEmail.current.value;
     const eventId = router?.query.id;
+
+    const validRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!emailValue.match(validRegex)) {
+      setMessage("Please add a correct email address");
+    }
 
     try {
       // POST fetch request
@@ -23,7 +31,8 @@ const SingleEvent = ({ data }) => {
 
       if (!response.ok) throw new Error(`error: ${response.status}`);
       const data = await response.json();
-      console.log("POST: ", data);
+      setMessage(data.message);
+      inputEmail.current.value = "";
     } catch (e) {
       console.log("ERROR", e);
     }
@@ -38,12 +47,13 @@ const SingleEvent = ({ data }) => {
       <form onSubmit={onSubmit} className="email_registration">
         <label>Get registered for this event!</label>
         <input
-          ref={inputEmail}
           type="email"
+          ref={inputEmail}
           id="email"
           placeholder="Please insert you email here"
         />
         <button type="submit">Submit</button>
+        <p>{message}</p>
       </form>
     </div>
   );
